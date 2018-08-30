6b1c968b0d3c4462ad54465863fb109e0a80677f
pflaz <pf@szach.net>
2018-08-30 23:01:08 +0200

    spring JPA static queries

---
+++ spring-jpa-queries-static/src/main/java/pl/javastart/model/Product.java
@@ -0,0 +1,91 @@
+package pl.javastart.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@NamedQueries({
+        @NamedQuery(name = "Product.findAll", query = "SELECT p FROM Product p"),
+        @NamedQuery(name = "Product.deleteAll", query = "DELETE FROM Product p")
+})
+
+public class Product implements Serializable {
+    private static final long serialVersionUID = 1;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String producer;
+    private Double price;
+
+    Product() {
+
+    }
+
+    public Product(String name, String producer, Double price) {
+        this.name = name;
+        this.producer = producer;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id) &&
+                Objects.equals(name, product.name) &&
+                Objects.equals(producer, product.producer) &&
+                Objects.equals(price, product.price);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, name, producer, price);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", producer='" + producer + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
