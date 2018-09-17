b417f65df5221d332f4d8f62462c954a07d7be6d
pflaz <pf@szach.net>
2018-09-17 21:54:45 +0200

    spring mvc data thymeleaf

---
+++ spring-mvc-data-thymeleaf/src/main/java/pl/javastart/SpringMvcDataThymeleafApplication.java
@@ -0,0 +1,12 @@
+package pl.javastart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcDataThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcDataThymeleafApplication.class, args);
+    }
+}
