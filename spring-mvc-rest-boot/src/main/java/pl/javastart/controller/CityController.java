7510aeae88f3760fb473bd10d3e27137da5a5243
pflaz <pf@szach.net>
2018-10-15 22:25:25 +0200

    spring mvc rest

--- spring-mvc-rest-boot/src/main/java/pl/javastart/controller/CityController.java
+++ spring-mvc-rest-boot/src/main/java/pl/javastart/controller/CityController.java
@@ -1,14 +1,63 @@
 package pl.javastart.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import pl.javastart.model.City;
 
+import java.net.URI;
+import java.util.*;
+
 @RestController
+@RequestMapping("/cities")
 public class CityController {
 
-    @RequestMapping("/city")
-    public City getCity() {
-        return new City("Warszawa", 1_700_700);
+    List<City> cities;
+
+    public CityController() {
+        cities = Collections.synchronizedList(new ArrayList<>());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<City> getCity(@PathVariable int id) {
+        if (id > cities.size() || id < 0) {
+            return ResponseEntity.notFound().build();
+        } else {
+            City city = cities.get(id);
+            return ResponseEntity.ok(city);
+        }
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<City> getCities(@RequestParam(defaultValue = "name") String orderBy) {
+        List<City> citiesCopy = new ArrayList<>(cities);
+
+        if (orderBy.equals("name")) {
+            citiesCopy.sort(Comparator.comparing(City::getName));
+        } else if (orderBy.equals("population")) {
+            citiesCopy.sort(Comparator.comparing(City::getPopulation));
+        }
+        return citiesCopy;
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<?> saveCity(@RequestBody City city) {
+        if (!cities.contains(city)) {
+            cities.add(city);
+            URI location = ServletUriComponentsBuilder
+                    .fromCurrentRequest()
+                    .path("/{id}")
+                    .buildAndExpand(cities.size() - 1)
+                    .toUri();
+            return ResponseEntity.created(location).body(city);
+        } else {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+
     }
 }
--- spring-mvc-rest-boot/src/main/java/pl/javastart/model/City.java
+++ spring-mvc-rest-boot/src/main/java/pl/javastart/model/City.java
@@ -1,30 +1,74 @@
 package pl.javastart.model;
 
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.Objects;
+
+@XmlRootElement
 public class City {
+    private int id;
     private String name;
     private int population;
+
+    public City() {
+    }
 
     public City(String name, int population) {
         this.name = name;
         this.population = population;
     }
 
-    public City() {
+    public City(int id, String name, int population) {
+        this.id = id;
+        this.name = name;
+        this.population = population;
     }
 
     public String getName() {
         return name;
     }
 
-    public int getPopulation() {
-        return population;
-    }
-
     public void setName(String name) {
         this.name = name;
     }
 
+    public int getPopulation() {
+        return population;
+    }
+
     public void setPopulation(int population) {
         this.population = population;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", population=" + population +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        City city = (City) o;
+        return id == city.id &&
+                population == city.population &&
+                Objects.equals(name, city.name);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, name, population);
     }
 }
