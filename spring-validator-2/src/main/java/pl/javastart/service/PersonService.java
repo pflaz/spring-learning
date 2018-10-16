be823bb4fbf5e033036891d06cc9ef39a21d417e
pflaz <pf@szach.net>
2018-10-16 15:33:03 +0200

    spring validator 2

---
+++ spring-validator-2/src/main/java/pl/javastart/service/PersonService.java
@@ -0,0 +1,44 @@
+package pl.javastart.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.Validator;
+import pl.javastart.model.Person;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class PersonService {
+
+    private Set<Person> people;
+
+    private Validator validator;
+
+    @Autowired
+
+    public PersonService(Validator validator) {
+        this.validator = validator;
+        this.people = new HashSet<>();
+    }
+
+    public void addPerson(Person person) {
+        Errors errors = new BeanPropertyBindingResult(person, "person");
+        validator.validate(person, errors);
+        if (errors.hasErrors()) {
+            System.err.printf("There are errors (%d): \n", errors.getErrorCount());
+            for (ObjectError error: errors.getAllErrors()) {
+                System.err.println(error.getDefaultMessage());
+            }
+        } else {
+            people.add(person);
+        }
+    }
+
+    public Set<Person> getPeople() {
+        return people;
+    }
+}
