34070b625ea3ddc7c2cf62ee15546cf940cffc79
pflaz <pf@szach.net>
2018-10-25 13:53:23 +0200

    spring + angular resource

--- spring-angular-http/src/main/resources/static/js/app.js
+++ spring-angular-http/src/main/resources/static/js/app.js
@@ -1,24 +1,31 @@
-angular.module('app', [])
-    .controller('ProductController', function($http) {
+angular.module('app', ['ngResource'])
+    .controller('ProductController', function($resource) {
         var vm = this;
+        var Product = $resource('api/products/:productId');
+        vm.product = new Product();
+
         function refreshData() {
-            $http.get('api/products')
-            .then(function success(response) {
-                vm.products = response.data;
-                console.log(vm.products);
-            }, function error(response) {
-                console.log('API error: ' + response.status);
-            });
+            vm.products = Product.query(
+                function success(data, headers) {
+                    console.log(data);
+                    console.log(headers('Content-Type'));
+                },
+                function error(response) {
+                    console.log('error: ' + response.status);
+                }
+            );
         }
 
         vm.addProduct = function(product) {
-            $http.post('api/products', product)
-                .then(function success(response) {
+            console.log(vm.product.__proto__);
+            vm.product.$save(function(data) {
                 refreshData();
-                vm.product = {};
-            }, function error(response) {
-                console.log('Data not saved ' + product);
+                vm.product = new Product();
             });
+        }
+
+        vm.loadData = function(id) {
+            vm.details = Product.get({productId:id});
         }
 
         vm.appName = 'Product Manager';
